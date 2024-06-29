e171d67a0f93637d236f5ac8502bd7f897324dbf
Matthias Eggert <matthias.eggert@qytera.de>
2024-06-29 22:02:03 +0200

    remove custom policy for s3

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -129,34 +129,6 @@ data "aws_ecs_task_execution" "loadtesting_task_execution" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attach" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-}
-
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "ecsS3AccessPolicy"
-  description = "Policy to allow ECS tasks to upload files to S3"
-  tags        = var.common_tags
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:PutObject",
-          "s3:PutObjectAcl",
-          "s3:PutObjectTagging",
-          "s3:PutObjectVersionAcl",
-          "s3:PutObjectVersionTagging"
-        ],
-        Resource = "arn:aws:s3:::${var.s3_bucket_name}/gatling-results/*"
-      }
-    ]
-  })
-}
-
 resource "aws_cloudwatch_log_group" "ecs_loadtesting" {
   name              = "/ecs/loadtesting"
   retention_in_days = 7
